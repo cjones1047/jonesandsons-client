fa61ae69ace36319ed41d1583a3e7d187cca7be8
cjones1047 <cjmj1047@gmail.com>
2022-09-24 20:04:56 -0500

    Imported getAllPictures() to Gallery.js, getAllPictures() correctly queries Django API for list of all pictures in database

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
+
+import { Spinner } from 'react-bootstrap';
+
+import { getAllPictures } from '../api/gallery';
 
 const Gallery = () => {
+
+    const [allPictures, setAllPictures] = useState(null)
+
+    useEffect(() => {
+        getAllPictures()
+            .then(res => setAllPictures(res.data))
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+
+    if(!allPictures) return (
+        <Spinner/>
+    )
+
     return(
         <div>
-            Gallery will go here.
+            {allPictures.length}
         </div>
     )
 }
